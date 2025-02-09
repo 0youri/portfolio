b37f95cdecd58ddbc47a81373c7ed92453f2af0f
Youri Novikov <73738882+0youri@users.noreply.github.com>
2025-02-09 14:31:27 +0100

    Update server.ts

--- backend/config/server.ts
+++ backend/config/server.ts
@@ -4,6 +4,6 @@ export default ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS'),
   },
-  // url: env('SERVER_URL', 'https://default-url.com'),
-  url: 'https://0youri.com/backend',
+  url: env('PUBLIC_URL', 'https://api.example.com'),
+  // url: 'https://0youri.com/backend',
 });
